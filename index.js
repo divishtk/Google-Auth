ffd8d0559be52258a92980e5dbda889db54bfdf8
divishtk <divisht.avion@hotmail.com>
2025-04-01 19:59:07 +0530

    Add MongoDB connection and user model; configure CORS and dotenv

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.utils.js";
+const app = express();
+const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+connectDB()
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
---
+++ models/user.models.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    verificationToken: {
+      type: String,
+    },
+    verificationTokenExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model("User", userSchema);
+export default user;
